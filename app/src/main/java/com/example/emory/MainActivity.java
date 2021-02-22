cbb95542e7c6ac4a55eceecae8a3fc535bd25ef3
Prologic <prologicnpl@gmail.com>
2021-02-22 18:13:50 +0545

    Two Ui developed

--- app/src/main/java/com/example/emory/MainActivity.java
+++ app/src/main/java/com/example/emory/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.emory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        btnSignUp=findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
