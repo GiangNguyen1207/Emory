cc8157f056f9bef810eb336b5e81f85fbee6255b
Subash KC <subash.k.c@metropolia.fi>
2021-02-23 17:01:44 +0200

    resolve all conflicts

--- app/src/main/java/com/example/emory/SignupActivity.java
+++ app/src/main/java/com/example/emory/SignupActivity.java
@@ -16,8 +16,8 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
     }
 }
