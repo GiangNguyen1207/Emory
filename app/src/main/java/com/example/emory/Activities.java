572a36394dc304db6af60fcdd9d97fa41344a7f4
My Mai <My.Mai@metropolia.fi>
2021-02-23 16:19:07 +0200

    updated version for UI of writing activity

--- app/src/main/java/com/example/emory/Activities.java
+++ app/src/main/java/com/example/emory/Activities.java
@@ -1,7 +1,13 @@
 package com.example.emory;
 
-public class Activities {
-    private String name;
-    private String icon;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class Activities extends AppCompatActivity {
+        @Override
+        protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_note);
+        }
 }
+
