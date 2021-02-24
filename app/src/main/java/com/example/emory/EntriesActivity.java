d94bfc34a986c3bcea6b6cebb2034df2daddef10
giangcn <giang.nguyen4@metropolia.fi>
2021-02-24 22:39:05 +0200

    add navigation

--- app/src/main/java/com/example/emory/EntriesActivity.java
+++ app/src/main/java/com/example/emory/EntriesActivity.java
@@ -1,14 +1,20 @@
 package com.example.emory;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EntriesActivity extends AppCompatActivity {
     private TextView month;
     private DayMonthYear monthYear;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +22,26 @@ public class EntriesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_entries);
 
         month = findViewById(R.id.month);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.entries);
 
         monthYear = new DayMonthYear();
         String currentMonthYear = monthYear.getCurrentMonthYear();
 
         month.setText(currentMonthYear);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.addMood:
+                        Intent intent = new Intent(getApplicationContext(), AddMoodActivity.class);
+                        startActivity(intent);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void onBack(View v) {
