fac2f2cb4ecdda4f508a70e27b2a86098f05c1ee
giangcn <giang.nguyen4@metropolia.fi>
2021-02-25 11:23:57 +0200

    configure data class

--- app/src/main/java/com/example/emory/Diary.java
+++ app/src/main/java/com/example/emory/Diary.java
@@ -1,34 +1,14 @@
 package com.example.emory;
 
-
 import java.util.ArrayList;
 
 public class Diary {
-    String date;
-    ArrayList<Mood> moods;
-    String note;
-    String pic;
+    String month;
+    ArrayList<SingleNote> notes;
 
-    public Diary(ArrayList<Mood>moods, String note, String pic) {
-        this.moods = moods;
-        this.note = note;
-        this.pic = pic;
-    }
-
-    public Diary(String date) {
-        this.date = date;
-    }
-
-    public String getDate() {
-        return this.date;
-    }
-
-    public String getNote() {
-        return this.note;
-    }
-
-    public String getPic() {
-        return this.pic;
+    public Diary(String month, ArrayList<SingleNote> notes) {
+        this.month = month;
+        this.notes = notes;
     }
 }
 
